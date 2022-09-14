b10c7a47ae4d18deabc150c737a2b1bc38ed9966
raprmdn <rafiputraramadhan14@gmail.com>
2022-09-14 11:18:27 +0700

    cookie, request body, static.

---
+++ test/cookie-signed.test.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import request from 'supertest';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+app.use(cookieParser('SECRET'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    const name = req.cookies['name'];
+    res.send(`Hello ${name}`);
+});
+
+app.post('/login', (req, res) => {
+    const name = req.body.name;
+    res.cookie('Login', name, { path: '/', signed: true });
+    res.send(`Hello ${name}`);
+});
+
+test('Test Cookie Read', async () => {
+    const response = await request(app).get('/')
+        .set('Cookie', 'name=Rafi;author=raprmdn');
+
+    expect(response.text).toBe('Hello Rafi');
+});
+
+test('Test Cookie Write', async () => {
+    const response = await request(app).post('/login')
+        .send({ name: 'Rafi' });
+
+    expect(response.text).toBe('Hello Rafi');
+    expect(response.get('Set-Cookie').toString()).toContain('Rafi');
+});
